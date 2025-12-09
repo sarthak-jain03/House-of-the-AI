59bd7e019d6fac1c20f814552bd21b5a3703b812
Sarthak Jain <sarthakjain4452@gmail.com>
2025-12-09 23:53:18 +0530

    Prepare inference for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080'}/api`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
